755c15d4957949dcdf451b5f2d10263db7b54e6c
BrayanGuerreroXD <bg7047653@gmail.com>
2023-02-27 18:42:43 -0500

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import CharacterList from "./components/CharacterList";
 
 function App() {
-
   return (
-    <div>
-      <h1>Rick and Morty</h1>
-      <CharacterList/>
+    <div className="bg-dark text-white">
+      <h1 className="text-center display-1 py-4">Rick and Morty</h1>
+      <CharacterList />
     </div>
   );
 }
